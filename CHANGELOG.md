21e16e465d0f9d9c92b90aeebda00a6bf71a18e7
Henrik Wassdahl <henrik.wassdahl@kidsloop.live>
2022-03-07 18:51:39 +0900

    chore(release): 1.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,13 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### [1.2.2](https://bitbucket.org/calmisland/kidsloop-cms-api-client/branches/compare/1.2.2%0D1.2.1) (2022-03-07)
+
+
+### 🐛 Bug Fixes
+
+* commit build artifacts when creating new version ([4e38e98](https://bitbucket.org/calmisland/kidsloop-cms-api-client/commits/4e38e9869ecb4ba336cb193bdc3e0c291117a0b9))
+
 ### [1.2.1](https://bitbucket.org/calmisland/kidsloop-cms-api-client/branches/compare/1.2.1%0D1.2.0) (2022-03-07)
 
 
--- dist/main.js
+++ dist/main.js
@@ -1,15 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ReactQueryDevtools = exports.useQueryClient = exports.useCmsApiClient = exports.CmsApiClientProvider = exports.useGetStudentAssessments = exports.usePostSchedulesTimeViewList = exports.usePostScheduleFeedback = exports.useGetScheduleNewestFeedbackById = exports.useGetScheduleById = exports.useGetLiveTokenByScheduleId = exports.useGetContentResourcePathById = void 0;
+exports.ReactQueryDevtools = exports.useQueryClient = exports.useCmsApiClient = exports.CmsApiClientProvider = exports.usePostSchedulesTimeViewList = exports.usePostScheduleFeedback = exports.useGetStudentAssessments = exports.useGetScheduleNewestFeedbackById = exports.useGetScheduleById = exports.useGetLiveTokenByScheduleId = exports.useGetContentResourcePathById = void 0;
 var content_1 = require("./api/content");
 Object.defineProperty(exports, "useGetContentResourcePathById", { enumerable: true, get: function () { return content_1.useGetContentResourcePathById; } });
 var schedule_1 = require("./api/schedule");
 Object.defineProperty(exports, "useGetLiveTokenByScheduleId", { enumerable: true, get: function () { return schedule_1.useGetLiveTokenByScheduleId; } });
 Object.defineProperty(exports, "useGetScheduleById", { enumerable: true, get: function () { return schedule_1.useGetScheduleById; } });
 Object.defineProperty(exports, "useGetScheduleNewestFeedbackById", { enumerable: true, get: function () { return schedule_1.useGetScheduleNewestFeedbackById; } });
+Object.defineProperty(exports, "useGetStudentAssessments", { enumerable: true, get: function () { return schedule_1.useGetStudentAssessments; } });
 Object.defineProperty(exports, "usePostScheduleFeedback", { enumerable: true, get: function () { return schedule_1.usePostScheduleFeedback; } });
 Object.defineProperty(exports, "usePostSchedulesTimeViewList", { enumerable: true, get: function () { return schedule_1.usePostSchedulesTimeViewList; } });
-Object.defineProperty(exports, "useGetStudentAssessments", { enumerable: true, get: function () { return schedule_1.useGetStudentAssessments; } });
 var core_1 = require("./core");
 Object.defineProperty(exports, "CmsApiClientProvider", { enumerable: true, get: function () { return core_1.CmsApiClientProvider; } });
 Object.defineProperty(exports, "useCmsApiClient", { enumerable: true, get: function () { return core_1.useCmsApiClient; } });
